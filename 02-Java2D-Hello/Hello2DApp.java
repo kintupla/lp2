dc101d45d51fe0810e5bf6301f9390aa6bdf3bdf
Matheus Honorato <75600932+kintupla@users.noreply.github.com>
2021-08-15 13:08:58 -0300

    Update Hello2DApp.java

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -17,19 +17,27 @@ class Hello2DFrame extends JFrame {
                 }
             }
         );
-        this.setTitle("Java2D - Hello World!");
-        this.setSize(450, 450);
+        this.setTitle("Java2D - Hello World! Modificado");
+        this.setSize(550, 550);
         this.setVisible(true);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
+        g2d.setPaint(Color.white);
         int w = getWidth();
         int h = getHeight();
         g2d.drawLine(0,0, w,h);
         g2d.drawLine(0,h, w,0);
-        getContentPane().setBackground(new Color(106,90,205));
+	    g.drawRect(getWidth()/10 , getHeight()/10,100,100);
+	    g.drawRect( (getWidth()/10) + 50, (getHeight()/10) + 50, 100, 100);
+	    g.drawLine(getWidth()/10, getHeight()/10, (getWidth()/10) + 50, (getHeight()/10) + 50);
+	    g.drawLine(getWidth()/10, (getHeight()/10) + 100, (getWidth()/10) + 50, (getHeight()/10) + 50 + 100);
+	    g.drawLine(  (getWidth()/10) + 100, (getHeight()/10) + 100, (getWidth()/10) + 100 + 50, (getHeight()/10) + 100 + 50);
+	    g.drawLine( (getWidth()/10) + 100, getHeight()/10, (getWidth()/10) + 50 + 100, (getHeight()/10) + 50);
+	    getContentPane().setBackground(new Color(106,90,205));
+	
+
     }
 }
