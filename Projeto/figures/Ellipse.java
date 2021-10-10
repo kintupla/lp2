eed612b42d794f33bd2f10024f67a6945ffd6c23
Matheus Honorato <75600932+kintupla@users.noreply.github.com>
2021-10-10 12:05:51 -0300

    Update Ellipse.java

--- Projeto/figures/Ellipse.java
+++ Projeto/figures/Ellipse.java
@@ -2,20 +2,77 @@ package figures;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Ellipse2D.Double;
 
 public class Ellipse extends Figure {
-    public Ellipse (int x, int y, int w, int h) {
-        super(x,y, w,h);
+    Ellipse2D ellipse;
+
+    public Ellipse(int x, int y, int width, int height, Color borderColor, Color fillColor) {
+        super(x, y, width, height, borderColor, fillColor);
+
+        this.ellipse = new Ellipse2D.Float(x, y, width, height);
     }
 
-    public void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+    public Ellipse(int x, int y, int width, int height) {
+        super(x, y, width, height, Color.BLACK, Color.WHITE);
+
+        this.ellipse = new Ellipse2D.Float(x, y, width, height);
     }
 
-    public void paint (Graphics g) {
+    @Override
+    public void Paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+
+        this.ellipse = new Ellipse2D.Double(this.x, this.y, this.width, this.height);
+
+        g2d.setStroke(new BasicStroke(defaultThickness * 3 / 2));
+
+        g2d.setColor(borderColor);
+        g2d.draw(ellipse);
+
+        g2d.setColor(fillColor);
+        g2d.fill(ellipse);
+    }
+
+    public boolean DentroFigura(Point mousePointPosition) {
+        return (mousePointPosition.x >= this.x) && (mousePointPosition.x <= this.x + this.width)
+                && (mousePointPosition.y >= this.y) && (mousePointPosition.y <= this.y + this.height);
+    }
+
+    public void RedSelection(Graphics g) {
+        super.RedSelection(g);
+    }
+
+    @Override
+    public void scale(int dx, int dy) {
+        this.width += dx;
+        this.height += dy;
+        if (this.width < 3) {
+            this.width -= dx;
+            this.height -= dy;
+        }
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        super.move(dx, dy);
+    }
+
+    @Override
+    public void dragFigure(Point mousePointPosition, int dx, int dy) {
+        Point pointToResize = new Point(this.x + this.width, this.y + this.height);
+
+        if (pointToResize.distance(mousePointPosition) <= 5) {
+            if (this.width + dx >= 10) {
+                this.width += dx;
+            }
+
+            if (this.height + dy >= 10) {
+                this.height += dy;
+            }
+        } else {
+            move(dx, dy);
+        }
+
+        this.ellipse.setFrame(this.x, this.y, this.width, this.height);
     }
 }
