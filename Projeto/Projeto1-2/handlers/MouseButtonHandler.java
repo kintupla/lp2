635d97f754603169e6e9a1c56979ee3d84617000
Matheus Honorato <75600932+kintupla@users.noreply.github.com>
2021-10-17 12:58:42 -0300

    Create MouseButtonHandler.java

---
+++ Projeto/Projeto1-2/handlers/MouseButtonHandler.java
@@ -0,0 +1,47 @@
+package handlers;
+
+import java.awt.event.*;
+import java.awt.Point;
+
+import java.util.ArrayList;
+
+import figures.*;
+
+public class MouseButtonHandler {
+    private static final int defaultSize = 100;
+
+    private class MouseButtons {
+        private static final int MOUSE1 = 1;
+    }
+
+    public static Figure SelectFigure(MouseEvent mouseEvent, ArrayList<Figure> figures, Figure selectedFigure) {
+        selectedFigure = null;
+
+        if (mouseEvent.getButton() == MouseButtons.MOUSE1) {
+            Point mousePointPosition = new Point(mouseEvent.getX(), mouseEvent.getY());
+
+            for (Figure figure : figures) {
+                if (figure.IsInsideFigure(mousePointPosition) == true) {
+                    selectedFigure = figure;
+                }
+            }
+
+            if (selectedFigure != null) {
+                figures.remove(selectedFigure);
+                figures.add(selectedFigure);
+            }
+        }
+
+        return selectedFigure;
+    }
+
+    public static Figure SelectAndDragFigure(MouseEvent mouseEvent, ArrayList<Figure> figures, Figure selectedFigure,
+            Point mousePointPosition) {
+        if (selectedFigure != null) {
+            selectedFigure.dragFigure(mousePointPosition, mouseEvent.getX() - mousePointPosition.x,
+                    mouseEvent.getY() - mousePointPosition.y);
+        }
+
+        return selectedFigure;
+    }
+}
