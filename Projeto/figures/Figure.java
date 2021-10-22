a71b07f2f8a2be7bb8979509dbf67f051a8aeab0
Matheus Honorato <75600932+kintupla@users.noreply.github.com>
2021-10-21 21:45:46 -0300

    Update Figure.java

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -7,15 +7,19 @@ import java.io.*;
 
 public abstract class Figure {
 	public int x, y;
-	public int width, height;
+	public int w, h;
 
-	public Figure(int x, int y, int width, int height) {
+	public Figure(int x, int y, int w, int h) {
 		this.x = x;
 		this.y = y;
-		this.width = width;
-		this.height = height;
+		this.w = w;
+		this.h = h;
 	}
 
 	public abstract void Paint(Graphics g);
+
+	public abstract void focusdafigura(Graphics g);
+
+	public abstract boolean clicado(Point mousePointPosition);
 
 }
