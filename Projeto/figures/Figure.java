c781eaee0580fef6e0f79558d930e44104a0e9a2
Matheus Honorato <75600932+kintupla@users.noreply.github.com>
2021-10-27 22:46:05 -0300

    Update Figure.java

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -1,20 +1,29 @@
 package figures;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.io.*;
 import interfaces.*;
 
 public abstract class Figure implements IVisible {
 	public int x, y;
 	public int w, h;
+	public int r, g, b;
+	public int rContorno, gContorno, bContorno;
+	public int Contorno;
 
-	public Figure(int x, int y, int w, int h) {
+	public Figure(int x, int y, int w, int h, int r, int g, int b, int rContorno, int gContorno, int bContorno,
+			int Contorno) {
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.rContorno = rContorno;
+		this.gContorno = gContorno;
+		this.bContorno = bContorno;
+		this.Contorno = Contorno;
 
 	}
 
@@ -32,4 +41,20 @@ public abstract class Figure implements IVisible {
 		this.y += DY;
 	}
 
+	public void AumentarOuDiminuir(int ADw, int ADh) {
+		this.w += ADw;
+		this.h += ADh;
+	}
+
+	public void PreenchimentoAleatorio(int r, int g, int b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
+	public void ContornoAleatorio(int rContorno, int gContorno, int bContorno) {
+		this.rContorno = rContorno;
+		this.gContorno = gContorno;
+		this.bContorno = bContorno;
+	}
 }
