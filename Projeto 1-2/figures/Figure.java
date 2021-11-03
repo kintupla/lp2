0ba5a4fa321b8442b32274cddac0dd7b33dd5d47
Matheus Honorato <75600932+kintupla@users.noreply.github.com>
2021-11-02 23:34:32 -0300

    Add files via upload

---
+++ Projeto 1-2/figures/Figure.java
@@ -0,0 +1,54 @@
+package figures;
+
+import java.awt.*;
+import java.awt.Graphics;
+import java.io.*;
+
+public abstract class Figure {
+	public int x, y;
+	public int w, h;
+	public int r, g, b;
+	public int rContorno, gContorno, bContorno;
+	public int Contorno;
+
+	public Figure(int x, int y, int w, int h, int r, int g, int b, int rContorno, int gContorno, int bContorno,
+			int Contorno) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.rContorno = rContorno;
+		this.gContorno = gContorno;
+		this.bContorno = bContorno;
+		this.Contorno = Contorno;
+
+	}
+
+	public abstract void Paint(Graphics g);
+
+	public abstract void focusdafigura(Graphics g);
+
+	public abstract boolean clicado(Point mousePointPosition);
+
+	public void drag(int DX, int DY) {
+		this.x += DX;
+		this.y += DY;
+	}
+
+	public abstract void AumentarOuDiminuir(int ADw, int ADh);
+
+	public void PreenchimentoAleatorio(int r, int g, int b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
+	public void ContornoAleatorio(int rContorno, int gContorno, int bContorno) {
+		this.rContorno = rContorno;
+		this.gContorno = gContorno;
+		this.bContorno = bContorno;
+	}
+}
---
+++ Projeto 1-2/figures/Rect.java
@@ -0,0 +1,41 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect extends Figure {
+
+    public Rect(int x, int y, int w, int h, int r, int g, int b, int rContorno, int gContorno, int bContorno,
+            int Contorno) {
+        super(x, y, w, h, r, g, b, rContorno, gContorno, bContorno, Contorno);
+    }
+
+    public void Paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(this.r, this.g, this.b));
+        g2d.fillRect(this.x, this.y, this.w, this.h);
+
+        g2d.setColor(new Color(this.gContorno, this.bContorno, this.Contorno));
+        g2d.setStroke(new BasicStroke(this.Contorno));
+        g2d.drawRect(this.x, this.y, this.w, this.h);
+
+    }
+
+    public boolean clicado(Point mousePointPosition) {
+        return (mousePointPosition.x <= this.x + this.w) && (mousePointPosition.x >= this.x)
+                && (mousePointPosition.y >= this.y) && (mousePointPosition.y <= this.y + this.h);
+    }
+
+    public void AumentarOuDiminuir(int ADw, int ADh) {
+        this.w += ADw;
+        this.h += ADh;
+    }
+
+    public void focusdafigura(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(3));
+        g2d.setColor(Color.RED);
+        g2d.drawRect(this.x, this.y, this.w, this.h);
+
+    }
+
+}
