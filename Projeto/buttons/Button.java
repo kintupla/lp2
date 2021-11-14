bbf67052f1f4c7b14d8315b4cd8e56b33f18c92c
Matheus Honorato <75600932+kintupla@users.noreply.github.com>
2021-11-14 19:26:49 -0300

    Create Button.java

---
+++ Projeto/buttons/Button.java
@@ -0,0 +1,45 @@
+package buttons;
+
+import java.awt.*;
+
+import interfaces.*;
+import figures.*;
+
+public class Button implements IVisible {
+    public int x, y;
+    public int buttonID;
+    public Figure figureButton;
+
+    public Button(int buttonID, Figure figureButton) {
+        this.figureButton = figureButton;
+        this.buttonID = buttonID;
+        this.y = 60 + 55 * buttonID;
+        this.x = 40;
+
+    }
+
+    public void Paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setStroke(new BasicStroke(3));
+        g2d.setPaint(Color.WHITE);
+        g2d.fillRect(this.x, this.y, 50, 50);
+
+        g2d.setPaint(Color.BLACK);
+        g2d.drawRect(this.x, this.y, 50, 50);
+
+        this.figureButton.Paint(g);
+    }
+
+    public boolean clicado(Point mousePointPosition) {
+        return (mousePointPosition.x >= this.x) && (mousePointPosition.x <= this.x + 50)
+                && (mousePointPosition.y >= this.y) && (mousePointPosition.y <= this.y + 50);
+    }
+
+    public void focusdafigura(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(4));
+        g2d.setColor(Color.RED);
+        g2d.drawRect(this.x, this.y, 50, 50);
+    }
+}
